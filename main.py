b16f977c089daec70a867ae880e9035e310e9c95
kush-hbk <kush.shah@hbkworlc.com>
2022-10-15 23:57:08 +0530

    created basic method for displaying the board like a laptop keypad

---
+++ main.py
@@ -0,0 +1,10 @@
+class Board:
+    def display_board(self): #Laptop number keypad 
+        print('',7,'|',8,'|',9,'')
+        print('---|---|---')
+        print('',4,'|',5,'|',6,'')
+        print('---|---|---')
+        print('',1,'|',2,'|',3,'')
+        print()
+board = Board()
+board.display_board()
